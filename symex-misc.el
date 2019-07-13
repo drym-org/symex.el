664dd66ec8974cacabad97cdf900f344b3df1bdb
Siddhartha <sid@countvajhula.com>
2019-07-13 14:34:16 -0700

    lint: remove unnecessary "Version" headers

--- symex-misc.el
+++ symex-misc.el
@@ -1,7 +1,6 @@
 ;;; symex-misc.el --- An evil way to edit Lisp symbolic expressions as trees -*- lexical-binding: t -*-
 
 ;; URL: https://github.com/countvajhula/symex-mode
-;; Version: 0.1
 ;; Package-Requires: ((emacs "24.4") (cl-lib "0.6.1") (lispy "0.26.0") (paredit "24") (evil-cleverparens "20170718.413") (dash-functional "2.15.0") (evil "20180914.1216") (smartparens "20181007.1501") (racket-mode "20181030.1345") (geiser "0.10") (evil-surround "20180102.1401") (hydra "20180918.1529"))
 
 ;; This program is "part of the world," in the sense described at
