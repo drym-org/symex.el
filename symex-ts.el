8a9085099b5f43553ba8c1d571bab59650fccac0
Simon Pugnet <simon@polaris64.net>
2021-10-30 16:27:20 +0200

    Add license text to symex-ts.el

--- symex-ts.el
+++ symex-ts.el
@@ -3,6 +3,23 @@
 ;; Author: Simon Pugnet <simon@polaris64.net>
 ;; URL: https://github.com/countvajhula/symex.el
 
+;; This program is "part of the world," in the sense described at
+;; https://drym.org.  From your perspective, this is no different than
+;; MIT or BSD or other such "liberal" licenses that you may be
+;; familiar with, that is to say, you are free to do whatever you like
+;; with this program.  It is much more than BSD or MIT, however, in
+;; that it isn't a license at all but an idea about the world and how
+;; economic systems could be set up so that everyone wins.  Learn more
+;; at drym.org.
+;;
+;; This work transcends traditional legal and economic systems, but
+;; for the purposes of any such systems within which you may need to
+;; operate:
+;;
+;; This is free and unencumbered software released into the public domain.
+;; The authors relinquish any copyright claims on this work.
+;;
+
 ;;; Commentary:
 
 ;; This module defines a set of movement primitives which use Tree
