457c7a8014aa380628b7c0bd34f6a282f820227a
Siddhartha <sid@countvajhula.com>
2022-09-15 12:57:57 -0700

    define "clear" using `symex-define-command`

--- symex-transformations.el
+++ symex-transformations.el
@@ -116,15 +116,12 @@
      (symex-select-nearest)
      (symex-tidy)))
 
-(defun symex-clear ()
+(symex-define-command symex-clear ()
   "Clear contents of symex."
   (interactive)
   (if (symex-tree-sitter-p)
       (symex-ts-clear)
-    (progn
-      (symex--clear)
-      (symex-select-nearest)
-      (symex-tidy))))
+    (symex--clear)))
 
 (defun symex--emit-backward ()
   "Emit backward."
