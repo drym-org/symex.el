051e197b88157ebd049ff2217af466b9e4b0f531
Siddhartha <sid@countvajhula.com>
2023-03-28 17:05:17 -0700

    use forward-line instead of previous-line (as suggested by docs)

--- symex-transformations-lisp.el
+++ symex-transformations-lisp.el
@@ -217,7 +217,7 @@ text, on the respective side."
           (save-excursion (forward-sexp)
                           (eobp)))
          (previous-line-empty
-          (save-excursion (previous-line)
+          (save-excursion (forward-line -1)
                           (symex--current-line-empty-p)))
          (next-line-empty
           (save-excursion (forward-sexp)
