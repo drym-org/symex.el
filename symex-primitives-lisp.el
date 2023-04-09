5748b19b18c813aef22c3a1eda78dbda5279d6f6
Siddhartha <sid@countvajhula.com>
2023-04-09 19:12:45 -0400

    don't change level in nearest selection

--- symex-primitives-lisp.el
+++ symex-primitives-lisp.el
@@ -355,7 +355,13 @@ as special cases here."
 
 (defun symex-lisp-select-nearest ()
   "Select the appropriate symex nearest to point."
-  (cond ((and (not (eobp))
+  (cond ((and (not (bobp))
+              (save-excursion (backward-char)
+                              (symex-left-p))
+              (not (eobp))
+              (symex-right-p)) ; (|)
+         nil) ; don't change level in selection
+        ((and (not (eobp))
               (symex-right-p)) ; |)
          (symex-other))
         ((condition-case nil  ; (thing-at-point string) raises error at EOB
