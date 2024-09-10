9ca1ddbf64b294e67436c9eee0f9cccf1c1ba145
Siddhartha <sid@countvajhula.com>
2024-09-09 18:39:59 -0700

    remove old comment

--- symex-misc.el
+++ symex-misc.el
@@ -489,7 +489,6 @@ ORIG-FN applied to ARGS is the invocation being advised."
 
 (defun symex-exit-mode ()
   "Take necessary action upon symex mode exit."
-  ;; these are "internal" state transitions, used in e.g. symex-evaluate
   (deactivate-mark)
   (when symex--original-blink-cursor-state
     (blink-cursor-mode 1))
