bd7b9273ab0e3dc49a3176806719fad91033fc46
Siddhartha <sid@countvajhula.com>
2025-04-07 18:32:03 -0700

    bump lithium dependency version 0.0 → 0.1

--- symex.el
+++ symex.el
@@ -3,7 +3,7 @@
 ;; Author: Siddhartha Kasivajhula <sid@countvajhula.com>
 ;; URL: https://github.com/drym-org/symex.el
 ;; Version: 1.0
-;; Package-Requires: ((emacs "25.1") (paredit "24") (seq "2.22") (lithium "0.0"))
+;; Package-Requires: ((emacs "25.1") (paredit "24") (seq "2.22") (lithium "0.1"))
 ;; Keywords: lisp, convenience, languages
 
 ;; This program is "part of the world," in the sense described at
