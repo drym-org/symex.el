1923b9b939fcb22f9f30d1a5a9d8da86bc996187
Siddhartha <sid@countvajhula.com>
2025-07-03 09:43:58 -0700

    add symex-evil and symex-rigpa modules
    The former was originally part of symex but was moved to a separate
    repo as part of making it a separate package, but that's not necessary
    to do. Instead it will be better to keep these modules in the common
    symex repo and just register them as different packages within it,
    while taking care to keep the dependencies isolated in some
    form (ideally keeping them in separate folders).

---
+++ symex-rigpa.el
@@ -0,0 +1,59 @@
+;;; symex-rigpa.el --- Use Symex with Rigpa -*- lexical-binding: t -*-
+
+;; Author: Siddhartha Kasivajhula <sid@countvajhula.com>
+;; URL: https://github.com/countvajhula/symex-rigpa
+;; Version: 0.0
+;; Package-Requires: ((emacs "25.1") (symex "2.0"))
+;; Keywords: lisp, convenience, languages
+
+;; This program is "part of the world," in the sense described at
+;; https://drym.org.  From your perspective, this is no different than
+;; MIT or BSD or other such "liberal" licenses that you may be
+;; familiar with, that is to say, you are free to do whatever you like
+;; with this program.  It is much more than BSD or MIT, however, in
+;; that it isn't a license at all but an idea about the world and how
+;; economic systems could be set up so that everyone wins.  Learn more
+;; at drym.org.
+;;
+;; This work transcends traditional legal and economic systems, but
+;; for the purposes of any such systems within which you may need to
+;; operate:
+;;
+;; This is free and unencumbered software released into the public domain.
+;; The authors relinquish any copyright claims on this work.
+;;
+
+;;; Commentary:
+
+;; Use Symex with Rigpa.
+
+;;; Code:
+
+(require 'rigpa)
+(require 'symex-mode)
+
+;; These override the definitions in the symex package, so this
+;; package (symex-evil) should be loaded after symex
+(defun symex-escape-higher ()
+  "Exit symex mode via an \"escape\"."
+  (interactive)
+  (rigpa-enter-higher-level))
+
+(defun symex-enter-lower ()
+  "Exit symex mode via an \"enter\"."
+  (interactive)
+  (rigpa-enter-lower-level))
+
+(defun symex-enter-lowest ()
+  "Enter the lowest (manual) editing level."
+  (interactive)
+  (rigpa-enter-lowest-level))
+
+;;;###autoload
+(defun symex-rigpa-initialize ()
+  "Rigpa interconnects for Symex."
+  nil)
+
+
+(provide 'symex-rigpa)
+;;; symex-rigpa.el ends here
