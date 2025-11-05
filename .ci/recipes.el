d406f779c1086068228f54b0b43670f1d12a3c09
Siddhartha <sid@countvajhula.com>
2025-11-04 16:22:41 -0800

    move project recipes into front-facing recipes.el

--- .ci/recipes.el
+++ .ci/recipes.el
@@ -1,44 +1,4 @@
-((symex-core
-  :repo nil
-  :host nil
-  :local-repo "."
-  :type git
-  :files ("symex-core/*.el"))
-
- (symex
-  :repo nil
-  :host nil
-  :local-repo "."
-  :type git
-  :files ("symex/*.el" "symex/doc/*.texi" "symex/doc/figures"))
-
- (symex-ide
-  :repo nil
-  :host nil
-  :local-repo "."
-  :type git
-  :files ("symex-ide/*.el"))
-
- (symex-evil
-  :repo nil
-  :host nil
-  :local-repo "."
-  :type git
-  :files ("symex-evil/*.el"))
-
- (symex-rigpa
-  :repo nil
-  :host nil
-  :local-repo "."
-  :type git
-  :files ("symex-rigpa/*.el"))
-
- (rigpa
-  :host github
-  :repo "countvajhula/rigpa"
-  :type git)
-
- (package-lint
+((package-lint
   :host github
   :repo "countvajhula/package-lint"
   ;; Ensure the necessary data files are included in the build.
---
+++ recipes.el
@@ -0,0 +1,34 @@
+((symex-core
+  :host github
+  :repo "drym-org/symex.el"
+  :type git
+  :files ("symex-core/*.el"))
+
+ (symex
+  :host github
+  :repo "drym-org/symex.el"
+  :type git
+  :files ("symex/*.el" "symex/doc/*.texi" "symex/doc/figures"))
+
+ (symex-ide
+  :host github
+  :repo "drym-org/symex.el"
+  :type git
+  :files ("symex-ide/*.el"))
+
+ (symex-evil
+  :host github
+  :repo "drym-org/symex.el"
+  :type git
+  :files ("symex-evil/*.el"))
+
+ (symex-rigpa
+  :host github
+  :repo "drym-org/symex.el"
+  :type git
+  :files ("symex-rigpa/*.el"))
+
+ (rigpa
+  :host github
+  :repo "countvajhula/rigpa"
+  :type git))
